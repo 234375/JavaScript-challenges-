6b23cdda19baf00b1762acc505cf91e578e1afd5
Sadya Ibrahim <sadyaibrahim791@gmail.com>
2026-03-29 06:57:56 +0300

    Add files via upload

---
+++ Challenge1.js
@@ -0,0 +1,7 @@
+//Write a function that takes an integer minutes and converts it to seconds.
+function convert(minutes){
+  sec = minutes * 60 ;
+  console.log(`${minutes} minute is ${sec} seconds`);
+}
+convert(5);
+
